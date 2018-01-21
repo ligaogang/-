843727c3ac87eb758f096ef88b0ab462be97d5c4
ligaogang <735450681@qq.com>
2018-01-21 21:11:17 +0800

    完成了面板的开发

---
+++ AugricultureNonPointSourcePollution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AugricultureNonPointSourcePollution.MvcApplication" Language="C#" %>
---
+++ AugricultureNonPointSourcePollution/Models/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AugricultureNonPointSourcePollution.Models
+{
+    //==============================================================
+    //  作者：李高钢  (735450681@qq.com)
+    //  时间：2018/1/21/周日 17:06:15
+    //  文件名：Class1
+    //  版本：V1.0.1  
+    //  说明： 
+    //  修改者：李高钢
+    //  修改说明： 
+    //==============================================================
+    public class Class1
+    {
+    }
+}
